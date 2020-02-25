a5fb46e1cbe4b79bcd8e2f9ed392d677f2147a99
Adithya Haridas <adithyah54@gmail.com>
2020-02-25 16:12:37 +0000

    Move custom syscall to own file

--- kernel/sys.c
+++ kernel/sys.c
@@ -2663,20 +2663,3 @@ COMPAT_SYSCALL_DEFINE1(sysinfo, struct compat_sysinfo __user *, info)
 	return 0;
 }
 #endif /* CONFIG_COMPAT */
-
-SYSCALL_DEFINE1(get_tag, int, givenPid)
-{
-	struct task_struct *item = find_task_by_vpid(givenPid);
-	if (!item) {
-		printk("get_tag called on nonexistant task_struct\n");
-		return -1;
-	}
-	printk("get_tag got tag of %d for pid = %d", item->tag, givenPid);
-	return 0;
-}
-
-SYSCALL_DEFINE2(set_tag, int, givenPid, unsigned int, tag)
-{
-	printk("set_tag syscall called...");
-	return 0;
-}
