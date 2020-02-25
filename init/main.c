28b2aee323e281b6a60e66f10ddbd28720e58914
Adithya Haridas <adithyah54@gmail.com>
2020-02-25 16:48:17 +0000

    Explicitly set tag to 0 for init process

--- init/main.c
+++ init/main.c
@@ -413,7 +413,7 @@ noinline void __ref rest_init(void)
 	 */
 	rcu_read_lock();
 	tsk = find_task_by_pid_ns(pid, &init_pid_ns);
-	printk("FUCKFUCK FUCK FUCK FUCK %d", pid);
+	tsk->tag = 0;
 	set_cpus_allowed_ptr(tsk, cpumask_of(smp_processor_id()));
 	rcu_read_unlock();
 
