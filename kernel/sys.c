06e8b5c722132058f06c29bd1fd0e1ba70e3d426
Adithya Haridas <adithyah54@gmail.com>
2020-02-25 03:33:08 +0000

    Fix var declr err (use struct)

--- kernel/sys.c
+++ kernel/sys.c
@@ -2666,7 +2666,7 @@ COMPAT_SYSCALL_DEFINE1(sysinfo, struct compat_sysinfo __user *, info)
 
 SYSCALL_DEFINE1(get_tag, int, givenPid)
 {
-	task_struct *item = find_task_by_vpid(givenPid);
+	struct task_struct *item = find_task_by_vpid(givenPid);
 	if (!item) {
 		printk("get_tag called on nonexistant task_struct\n");
 		return -1;
