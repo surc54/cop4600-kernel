296a0881d538ec909157e88c2b20a7ea141cea0f
Adithya Haridas <adithyah54@gmail.com>
2020-02-25 16:52:32 +0000

    Change tag data type to unsigned int

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1214,7 +1214,7 @@ struct task_struct {
 	 * New fields for task_struct should be added above here, so that
 	 * they are included in the randomized portion of task_struct.
 	 */
-	int				tag;
+	unsigned int			tag;
 	randomized_struct_fields_end
 
 	/* CPU-specific state of this task: */
