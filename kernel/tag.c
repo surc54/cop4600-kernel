9ffa36ce21c1fd78fbc823548e3fcc6b0c5de9f1
Adithya Haridas <adithyah54@gmail.com>
2020-02-29 17:36:48 +0000

    Fix bug where dmesg doesn't show last message

--- kernel/tag.c
+++ kernel/tag.c
@@ -7,10 +7,10 @@ SYSCALL_DEFINE1(get_tag, int, givenPid)
 {
         struct task_struct *item = find_task_by_vpid(givenPid);
         if (!item) {
-                printk("get_tag called on nonexistant pid %d", givenPid);
+                printk("get_tag called on nonexistant pid %d\n", givenPid);
                 return -1;
         }
-        printk("get_tag got tag of %d for pid = %d", item->tag, givenPid);
+        printk("get_tag got tag of %d for pid = %d\n", item->tag, givenPid);
         return item->tag;
 }
 
@@ -28,7 +28,7 @@ SYSCALL_DEFINE2(set_tag, int, pid, unsigned int, tag)
 		check;
 
 	if (tag >> 31 != 0) {
-		printk("set_tag called with invalid tag (not 0 in MSB)");
+		printk("set_tag called with invalid tag (not 0 in MSB)\n");
 		return -1;
 	}
 
@@ -38,18 +38,18 @@ SYSCALL_DEFINE2(set_tag, int, pid, unsigned int, tag)
 	if (current->pid != pid) {
 		// Modifying someone else
 		if (!is_root) {
-			printk("set_tag called without sudo and non-current pid");
+			printk("set_tag called without sudo and non-current pid\n");
 			return -1;
 		}
 
 		item = find_task_by_vpid(pid);
 
 		if (!item) {
-			printk("set_tag called on nonexistant pid %d", pid);
+			printk("set_tag called on nonexistant pid %d\n", pid);
 			return -1;
 		}
 
-		printk("set_tag on PID %d (%u -> %u)", pid, item->tag, tag);
+		printk("set_tag on PID %d (%u -> %u)\n", pid, item->tag, tag);
 		item->tag = tag;
 	} else {
 		// Modifying self
@@ -59,7 +59,7 @@ SYSCALL_DEFINE2(set_tag, int, pid, unsigned int, tag)
 			cur_level = current_tag & 3;
 			new_level = tag & 3;
 			if (new_level > cur_level) {
-				printk("set_tag called without sudo and tag with higher level (%u -> %u)",
+				printk("set_tag called without sudo and tag with higher level (%u -> %u)\n",
 					cur_level, new_level);
 				return -1;
 			}
@@ -71,13 +71,13 @@ SYSCALL_DEFINE2(set_tag, int, pid, unsigned int, tag)
 			check = diff & cur_bitmap;
 
 			if (diff != check) {
-				printk("set_tag called without sudo and tag bitmap item set to 1");
+				printk("set_tag called without sudo and tag bitmap item set to 1\n");
 				return -1;
 			}
 		}
 		// everything is okay, set tag
 		current->tag = tag;
-		printk("set_tag on PID %d (%u -> %u)", pid, current_tag, tag);
+		printk("set_tag on PID %d (%u -> %u)\n", pid, current_tag, tag);
 	}
         return 0;
 }
